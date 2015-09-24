8df457fd245172f9a38e9615bdb1a26c5d6a7d27
Manuel Stich <manuel_stich@gmx.de>
2015-09-24 18:43:01 +0200

    Done my implementation

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M=matrix()) {
+  #Initialize inverse Matrix of M: TempInvOfM
+  TempInvOfM <- NULL
+  set <- function(Y) {
+    M <<- Y
+    TempInvOfM <<- NULL
+  }
+  get <- function() M
+  setinv <- function(M_inv) TempInvOfM <<- M_inv
+  getinv <- function() TempInvOfM
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M, ...) {
+  TempInvOfM <- M$getinv()
+  if(!is.null(TempInvOfM)) {
+    message("getting cached data")
+    return(TempInvOfM)
+  }
+  data <- M$get()
+  TempInvOfM <- solve(data, ...)
+  M$setinv(TempInvOfM)
+  TempInvOfM
 }
