8ba73a3e8bc96d9e0d5fa49a2eff6d6ed0fbcb8a
Manuel Stich <manuel_stich@gmx.de>
2015-09-24 19:14:00 +0200

    Done my implementation with valid comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,16 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(M=matrix()) {
+  #Input: Matrix M
+  #Output: Cached Matrix 
+  #Function: Generates a cache Matrix of the Matrix M
+  
   #Initialize inverse Matrix of M: TempInvOfM
   TempInvOfM <- NULL
+  #Implementation of the Setter function
   set <- function(Y) {
-    M <<- Y
-    TempInvOfM <<- NULL
+    M <<- Y #M takes value of Y <<- Operator used to acces environment
+    TempInvOfM <<- NULL #Null Pointer
   }
-  get <- function() M
-  setinv <- function(M_inv) TempInvOfM <<- M_inv
+  get <- function() M #get Value for M
+  setinv <- function(M_inv) TempInvOfM <<- M_inv #set the inverse of M
   getinv <- function() TempInvOfM
-  list(set = set, get = get,
+  list(set = set, get = get, #Save all functions in a list
        setinv = setinv,
        getinv = getinv)
 }
@@ -21,13 +26,17 @@ makeCacheMatrix <- function(M=matrix()) {
 
 
 cacheSolve <- function(M, ...) {
-  TempInvOfM <- M$getinv()
-  if(!is.null(TempInvOfM)) {
-    message("getting cached data")
+  #Input: Matrix M (e.g. Output of makeCacheMatrix)
+  #Output: Inverse Matrix of M
+  #Function: Generates a Matrix inverse of M
+  
+  TempInvOfM <- M$getinv() #access list 
+  if(!is.null(TempInvOfM)) { #if TempInvOfM points something
+    message("getting cached data") 
     return(TempInvOfM)
   }
-  data <- M$get()
+  data <- M$get() #Get the get()-function of the list
   TempInvOfM <- solve(data, ...)
-  M$setinv(TempInvOfM)
+  M$setinv(TempInvOfM) #setting data
   TempInvOfM
 }
